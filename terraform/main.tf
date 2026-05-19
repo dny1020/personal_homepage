acfd7a82d06ada0082e4cc54634ace46ecf07707
Danilo Narvaez <jnarvaar@icloud.com>
2026-05-19 13:30:21 -0500

    Update Terraform with SSH keys and security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,14 +22,60 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+# Par de claves SSH
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIATZ3hKAciqQqpn2uL3Den2Ql5mre4QmSmZTrvKiils jnarvaar@icloud.com"
+}
+
 # Solicitud de Instancia EC2 Spot
 resource "aws_spot_instance_request" "spot_instance" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.micro"  # La menor capacidad recomendada
-  spot_type     = "one-time"  # Se cerrará la solicitud si la apagas o es interrumpida
+  instance_type = "t3.micro"
+  spot_type     = "one-time"
+  key_name      = aws_key_pair.deployer.key_name
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "MiInstanciaSpot"
   }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_access"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_spot_instance_request.spot_instance.public_ip
 }
 
